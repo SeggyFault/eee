01da6b09664ea1b2bf9e7f4cc5fd53e2c203d375
sggyflt <sgflt88@gmail.com>
2024-08-01 16:25:26 +0200

    feat: add project custom post type

---
+++ wp-content/themes/sc-starter-theme/archive-project.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The Index Template File.
+ *
+ * @package sc-starter-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Starter_Theme;
+
+use Timber\Timber as TimberLibrary;
+
+$context = TimberLibrary::context();
+$context['projects'] = TimberLibrary::get_posts(
+  array(
+    'post_type' => 'project',
+    'posts_per_page' => 1,
+    'paged' => get_query_var( 'paged' )
+  )
+);
+
+TimberLibrary::render( 'archive-project.twig', $context );
--- wp-content/themes/sc-starter-theme/src/custom-post-types/post-types/class-project.php
+++ wp-content/themes/sc-starter-theme/src/custom-post-types/post-types/class-project.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Contains Somoscuatro\Starter_Theme\Custom_Post_Types\Post_Types\Samples Class.
+ * Contains Somoscuatro\Starter_Theme\Custom_Post_Types\Post_Types\Project Class.
  *
  * @package sc-starter-theme
  */
@@ -12,23 +12,23 @@ namespace Somoscuatro\Starter_Theme\Custom_Post_Types\Post_Types;
 use Somoscuatro\Starter_Theme\Custom_Post_Types\Custom_Post_Type;
 
 /**
- * Sample Custom Post Type Functionality.
+ * Project Custom Post Type Functionality.
  */
-class Sample extends Custom_Post_Type {
+class Project extends Custom_Post_Type {
 
 	/**
 	 * Custom Post Type Singular Name.
 	 *
 	 * @var string
 	 */
-	protected string $singular_name = 'Sample';
+	protected string $singular_name = 'Project';
 
 	/**
 	 * Custom Post Type Plural Name.
 	 *
 	 * @var string
 	 */
-	protected string $plural_name = 'Samples';
+	protected string $plural_name = 'Projects';
 
 	/**
 	 * Class Constructor.
@@ -41,8 +41,8 @@ class Sample extends Custom_Post_Type {
 			'menu_icon'    => 'data:image/svg+xml;base64,' . base64_encode(
 				'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"> <path fill-rule="evenodd" d="M10.5 3.798v5.02a3 3 0 0 1-.879 2.121l-2.377 2.377a9.845 9.845 0 0 1 5.091 1.013 8.315 8.315 0 0 0 5.713.636l.285-.071-3.954-3.955a3 3 0 0 1-.879-2.121v-5.02a23.614 23.614 0 0 0-3 0Zm4.5.138a.75.75 0 0 0 .093-1.495A24.837 24.837 0 0 0 12 2.25a25.048 25.048 0 0 0-3.093.191A.75.75 0 0 0 9 3.936v4.882a1.5 1.5 0 0 1-.44 1.06l-6.293 6.294c-1.62 1.621-.903 4.475 1.471 4.88 2.686.46 5.447.698 8.262.698 2.816 0 5.576-.239 8.262-.697 2.373-.406 3.092-3.26 1.47-4.881L15.44 9.879A1.5 1.5 0 0 1 15 8.818V3.936Z" clip-rule="evenodd" /> </svg>'
 			),
-			'has_archive'  => false,
-			'supports'     => array( 'title', 'editor', 'revisions' ),
+			'has_archive'  => true,
+			'supports'     => array( 'title', 'editor', 'revisions', 'thumbnail' ),
 			'show_in_rest' => true,
 		);
 
