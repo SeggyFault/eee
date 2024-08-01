705abfcd4a9af956bc3667a7f7bfc45541f04470
sggyflt <sgflt88@gmail.com>
2024-08-01 16:26:10 +0200

    fix: increase projects in archive page to 10

--- wp-content/themes/sc-starter-theme/archive-project.php
+++ wp-content/themes/sc-starter-theme/archive-project.php
@@ -15,7 +15,7 @@ $context = TimberLibrary::context();
 $context['projects'] = TimberLibrary::get_posts(
   array(
     'post_type' => 'project',
-    'posts_per_page' => 1,
+    'posts_per_page' => 9,
     'paged' => get_query_var( 'paged' )
   )
 );
