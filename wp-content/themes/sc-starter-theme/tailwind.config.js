2fe7844e8188dd5b59a45ee3af4a1315f59f8aa1
sggyflt <sgflt88@gmail.com>
2024-08-08 16:46:09 +0200

    feat: add urbanist font

--- wp-content/themes/sc-starter-theme/tailwind.config.js
+++ wp-content/themes/sc-starter-theme/tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				ebgaramond: ['EBGaramond', 'Times New Roman', 'serif'],
+        urbanist: ['Urbanist', 'Helvetica Neue', 'Arial', 'sans-serif'],
 			},
 			colors: tailwindColorPalette,
       backgroundImage: {
