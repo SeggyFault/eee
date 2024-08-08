83bcf35802dd78b7a40a5362e7e3173913fe2dc2
sggyflt <sgflt88@gmail.com>
2024-08-08 16:49:23 +0200

    fix: global context not accessible from blocks

--- wp-content/themes/sc-starter-theme/src/blocks/class-block.php
+++ wp-content/themes/sc-starter-theme/src/blocks/class-block.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Somoscuatro\Starter_Theme\Blocks;
 
 use Somoscuatro\Starter_Theme\Timber;
+use Timber\Timber as TimberLibrary;
 
 /**
  * Class for ACF Gutenberg Blocks.
@@ -138,13 +139,13 @@ class Block implements Block_Interface {
 	public function set_context( array $context, array $block, bool $is_preview ): array {
 		unset( $block['data'] );
 		$context['block'] = $block;
-
+    
 		$fields = get_fields();
 
 		if ( $fields ) {
-			$context = $this->add_acf_fields_to_context( static::$acf_block_prefix, $context, $fields );
+      $context = $this->add_acf_fields_to_context( static::$acf_block_prefix, $context, $fields );
 		}
-
+    
 		$context['is_preview'] = $is_preview;
 
 		return $this->set_custom_context( $context, $block );
