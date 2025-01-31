530c9de65d482f91ae2b629de2ebd045c922a106
sggyflt <sgflt88@gmail.com>
2025-01-31 18:00:24 +0100

    feat: add autoplay to slider

--- wp-content/themes/eee-theme/src/blocks/slider/slider.ts
+++ wp-content/themes/eee-theme/src/blocks/slider/slider.ts
@@ -1,2 +1,6 @@
 // @ts-ignore
-new Glide('.glide').mount();
+new Glide('.glide', {
+    autoplay: 3000,
+    hoverpause: false,
+    animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)'
+}).mount();
